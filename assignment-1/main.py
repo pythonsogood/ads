f1e5b8d41386b26412f37dd9bb325dd9235413b8
pythonsogood <181958582+pythonsogood@users.noreply.github.com>
2025-03-20 13:43:57 +0500

    initial commit

---
+++ assignment-1/main.py
@@ -0,0 +1,41 @@
+def task_1(n: int) -> int:
+	return n ** 2 + task_1(n-1) if n != 1 else 1
+
+def task_2(n: int, arr: list[int]) -> int:
+	return (arr[n-1] if len(arr) > n else 0) + task_2(n-1, arr) if n > 0 else 0
+
+def task_3(n: int) -> int:
+	return n + task_3(n-1) if n > 0 else 0
+
+def task_4(n: int, b: int) -> int:
+	return b ** n + task_4(n-1, b) if n >= 0 else 0
+
+def task_5(n: int, *seq: tuple[int, ...]) -> str:
+	if n > 0:
+		a, b = seq[n-1], task_5(n-1, *seq)
+		return f"{a} {b}" if len(b) > 0 else f"{a}"
+	return ""
+
+def task_6(n: int, *strs: tuple[str, ...]) -> str:
+	if n > 0:
+		a, b = strs[n-1], task_6(n-1, *strs)
+		return f"{a}\n{b}" if len(b) > 0 else f"{a}"
+	return ""
+
+def task_7(n: int, _row: int=0) -> str:
+	if _row > n :
+		return ""
+	res = ""
+	for i in (range(n) if _row % 2 == 0 else range(n-1, -1, -1)):
+		res += str(i + n * _row) + " "
+	return res + "\n" + task_7(n, _row+1) if _row+1 < n else res
+
+
+if __name__ == "__main__":
+	print(task_1(4))
+	print(task_2(4, [1, 2, 3, 4, 5]))
+	print(task_3(4))
+	print(task_4(3, 4))
+	print(task_5(3, 1, 2, 3))
+	print(task_6(3, "Abc", "bcdh", "abcdef"))
+	print(task_7(3))
